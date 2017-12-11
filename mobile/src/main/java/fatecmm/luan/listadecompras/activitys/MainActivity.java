b8962dd0dea7f1e07ceeb33feffa6360657b9bb3
Luan Carleti <luan.alves2@fatec.sp.gov.br>
2017-12-10 22:31:04 -0200

    Second Commit Test

--- mobile/src/main/java/fatecmm/luan/listadecompras/activitys/MainActivity.java
+++ mobile/src/main/java/fatecmm/luan/listadecompras/activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package fatecmm.luan.listadecompras;
+package fatecmm.luan.listadecompras.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import fatecmm.luan.listadecompras.R;
 
 public class MainActivity extends AppCompatActivity {
 
