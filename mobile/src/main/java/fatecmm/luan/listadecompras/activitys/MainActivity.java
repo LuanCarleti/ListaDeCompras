c89732f0437bb4cfcf018902de5391964e430869
Luan Carleti <luan.alves2@fatec.sp.gov.br>
2017-12-15 06:43:53 -0200

    Last test commit

--- mobile/src/main/java/fatecmm/luan/listadecompras/activitys/MainActivity.java
+++ mobile/src/main/java/fatecmm/luan/listadecompras/activitys/MainActivity.java
@@ -1,15 +1,20 @@
-package fatecmm.luan.listadecompras.Activity;
+package fatecmm.luan.listadecompras.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import fatecmm.luan.listadecompras.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textView1);
     }
 }
